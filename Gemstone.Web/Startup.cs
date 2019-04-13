56d18b0eda577e2e036c4da5076ce50c383e6041
Rafal <rafal.kalisz.dev@gmail.com>
2019-04-13 23:48:51 +0200

    added API controller

--- Gemstone.Web/Startup.cs
+++ Gemstone.Web/Startup.cs
@@ -5,29 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Gemstone
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
-        
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             // first commit 2019_04_11 23:49
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
