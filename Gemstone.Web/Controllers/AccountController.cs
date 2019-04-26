b1e8fe258e943ddb2ff4e3e7e31df804248c12a0
Rafal <rafal.kalisz.dev@gmail.com>
2019-04-26 23:35:00 +0200

    add AccountController

---
+++ Gemstone.Web/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gemstone.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gemstone.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var model = new AccountModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Register(AccountModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gemstone.Web/Models/AccountModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gemstone.Web.Models
+{
+    public class AccountModel
+    {
+        public string Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
