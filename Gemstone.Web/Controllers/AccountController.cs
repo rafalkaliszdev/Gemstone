2369a5108be80e171e1b208cee2eb0fdbf327e72
Rafal <rafal.kalisz.dev@gmail.com>
2019-04-27 22:50:58 +0200

    basic authenciation

--- Gemstone.Web/Controllers/AccountController.cs
+++ Gemstone.Web/Controllers/AccountController.cs
@@ -5,6 +5,14 @@ using System.Threading.Tasks;
 using Gemstone.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+
 namespace Gemstone.Web.Controllers
 {
     public class AccountController : Controller
@@ -12,6 +20,9 @@ namespace Gemstone.Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
+            var q = HttpContext.User.Claims.ToList();
+            return Content("your log status: " + q.Count);
+
             var model = new AccountModel();
             return View(model);
         }
@@ -25,12 +36,25 @@ namespace Gemstone.Web.Controllers
         [HttpGet]
         public IActionResult Login()
         {
+            //create principal for the current authentication scheme
+            var claims = new List<Claim>
+            {
+                new Claim("Name","testUser1"),
+                new Claim("Email","test@test.pl")
+            };
+
+            var userIdentity = new ClaimsIdentity(claims, "Auth");
+            var userPrincipal = new ClaimsPrincipal(userIdentity);
+
+            HttpContext.SignInAsync("Auth", userPrincipal);
+
             return View();
         }
 
         [HttpGet]
         public IActionResult Logout()
         {
+            HttpContext.SignOutAsync("Auth");
             return View();
         }
     }
