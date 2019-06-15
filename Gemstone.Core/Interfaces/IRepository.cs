f29bd0490328c56cb8bf6fccfa87feda4020afa1
Rafal <rafal.kalisz.dev@gmail.com>
2019-06-15 23:33:16 +0200

    async generic repository

--- Gemstone.Core/Interfaces/IRepository.cs
+++ Gemstone.Core/Interfaces/IRepository.cs
@@ -14,4 +14,15 @@ namespace Gemstone.Core.Interfaces
         Task UpdateAsync(TEntity entity);
         Task DeleteAsync(TEntity model);
     }
+
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<int> CountAsync(ISpecification<T> spec);
+    }
 }
