9c70fb012c12f637b1b4f7a4d05c4131c17d9adb
Rafal <rafal.kalisz.dev@gmail.com>
2019-06-18 23:52:15 +0200

    AccountRepository

--- Gemstone.Infrastructure/AccountRepository.cs
+++ Gemstone.Infrastructure/AccountRepository.cs
@@ -8,51 +8,51 @@ using System.Threading.Tasks;
 
 namespace Gemstone.Infrastructure
 {
-    public class AccountRepository : IAsyncRepository<Account>
+    public class AccountRepository : EfRepository<Assignment>, IAccountRepository
     {
         private readonly EfDbContext _context;
 
-        public AccountRepository(EfDbContext context)
+        public AccountRepository(EfDbContext context) : base(context)
         {
             _context = context;
         }
 
-        public async Task<IReadOnlyCollection<Account>> ListAllAsync()
-        {
-            var records = await (from acc in _context.Account
-                                 select acc).AsNoTracking().ToListAsync();
-            return records.AsReadOnly();
-        }
+        //public async Task<IReadOnlyCollection<Account>> ListAllAsync()
+        //{
+        //    var records = await (from acc in _context.Account
+        //                         select acc).AsNoTracking().ToListAsync();
+        //    return records.AsReadOnly();
+        //}
 
-        public async Task<Account> GetByIdAsync(long id)
-        {
-            var record = await (from acc in _context.Account
-                                where acc.ID == id
-                                select acc ).SingleOrDefaultAsync();
-            return record;
-        }
+        //public async Task<Account> GetByIdAsync(long id)
+        //{
+        //    var record = await (from acc in _context.Account
+        //                        where acc.ID == id
+        //                        select acc ).SingleOrDefaultAsync();
+        //    return record;
+        //}
 
-        public async Task AddAsync(Account entity)
-        {
-            await _context.Account.AddAsync(entity);
-            await _context.SaveChangesAsync();
-        }
+        //public async Task AddAsync(Account entity)
+        //{
+        //    await _context.Account.AddAsync(entity);
+        //    await _context.SaveChangesAsync();
+        //}
 
-        public async Task UpdateAsync(Account entity)
-        {
-            _context.Account.Update(entity);
-            await _context.SaveChangesAsync();
-        }
+        //public async Task UpdateAsync(Account entity)
+        //{
+        //    _context.Account.Update(entity);
+        //    await _context.SaveChangesAsync();
+        //}
 
-        public async Task DeleteAsync(Account model)
-        {
-            _context.Account.Remove(model);
-            await _context.SaveChangesAsync();
-        }
+        //public async Task DeleteAsync(Account model)
+        //{
+        //    _context.Account.Remove(model);
+        //    await _context.SaveChangesAsync();
+        //}
 
-        public async Task<bool> ExistsById(long id)
-        {
-            return await _context.Account.AnyAsync(p => p.ID == id);
-        }
+        //public async Task<bool> ExistsById(long id)
+        //{
+        //    return await _context.Account.AnyAsync(p => p.ID == id);
+        //}
     }
 }
